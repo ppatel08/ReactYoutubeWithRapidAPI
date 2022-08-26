7d0da4efe014cd4f45c315b15e1f9583d12348c6
ppatel08 <noreplyonlyspam@gmail.com>
2022-08-26 12:02:00 -0400

    commiting the sequence

--- tube-rapid-api/src/App.js
+++ tube-rapid-api/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Priyank Test.
+          Edit <code>src/App.js</code> This is another test.
         </p>
         <a
           className="App-link"
